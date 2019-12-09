182d312a279e3fea5add15b4d41bd32451a2dfc6
Monia-L <monia.laoufi@gmail.com>
2019-12-09 21:50:57 +0100

    Deploy master branches to URL named after branch

---
+++ scripts/deploy_staging_app.sh
@@ -0,0 +1,4 @@
+echo "{ "alias": "staging-ml" }" > now-staging-app.json
+now --public --token $NOW_TOKEN
+now alias --token $NOW_TOKEN --local-config now-staging-app.json
+#Déploiement automatique de la branche master en pré-production
