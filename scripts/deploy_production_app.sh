68f3cdc8c8c3be764978bd6b03ea52523833d8e8
Monia-L <monia.laoufi@gmail.com>
2019-12-10 11:12:08 +0100

    fix bug on JSON file

--- scripts/deploy_production_app.sh
+++ scripts/deploy_production_app.sh
@@ -1,3 +1,4 @@
-echo "{ "alias": "production-ml" }" > now-production-app.json
+echo "{ \"alias\": \"production-ml\" }" > now-production-app.json
+
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-production-app.json
